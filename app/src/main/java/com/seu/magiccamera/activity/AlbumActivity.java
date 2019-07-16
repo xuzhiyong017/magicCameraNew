a0f3605f5feedad43285ad11cffc49010e65c9df
912696973 <912696973@qq.com>
2019-07-16 18:08:40 +0800

    init project

---
+++ app/src/main/java/com/seu/magiccamera/activity/AlbumActivity.java
@@ -0,0 +1,21 @@
+package com.seu.magiccamera.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.seu.magiccamera.R;
+
+/**
+ * Created by why8222 on 2016/3/18.
+ */
+public class AlbumActivity extends Activity{
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_page);
+    }
+}
